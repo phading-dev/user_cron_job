f7520b72faecee641daaa365fa053beed81f61ce
Ykuyo <ykuyo.dev@gmail.com>
2025-07-26 14:48:03 -0700

    Added prod env.

---
+++ prod/turnup.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+# GCP auth
+gcloud auth application-default login
+gcloud config set project phading-prod
+
+# Create service account
+gcloud iam service-accounts create user-cron-builder
+
+# Grant permissions to the service account
+gcloud projects add-iam-policy-binding phading-prod --member="serviceAccount:user-cron-builder@phading-prod.iam.gserviceaccount.com" --role='roles/cloudbuild.builds.builder' --condition=None
+gcloud projects add-iam-policy-binding phading-prod --member="serviceAccount:user-cron-builder@phading-prod.iam.gserviceaccount.com" --role='roles/container.developer' --condition=None
+
+# Create the service account
+kubectl create serviceaccount user-cron-account --namespace default
